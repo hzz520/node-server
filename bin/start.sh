c9a0b64941279188208980045c6ed5080a3ff774
huangzhongzhen <1013452861@qq.com>
2019-10-03 14:55:49 +0800

    hzz

--- bin/start.sh
+++ bin/start.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 if [ -d "/opt/node-server/build" ] && [ -f "/opt/node-server/build/index.js" ];then
-    cd '/opt/node-server/build'
+    cd '/opt/node-server'
     cnpm i
     if lsof -i tcp:8001;then
 	    echo 'restart node-server'
-        pm2 restart "./index.js"
+        pm2 restart "./build/index.js"
     else
 	    echo 'start node-server'
-        pm2 start -n node-server -f "./index.js"
+        pm2 start -n node-server -f "./build/index.js"
     fi
 else
    echo 'error'
