a4384abd124357cc3284c9e5953a6355cc410216
huangzhongzhen <1013452861@qq.com>
2019-10-03 14:53:34 +0800

    hzz

--- bin/start.sh
+++ bin/start.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 if [ -d "/opt/node-server/build" ] && [ -f "/opt/node-server/build/index.js" ];then
-    if [ ! which pm2 ];then
-        cnpm i -g pm2
-    fi
     cd '/opt/node-server/build'
     cnpm i
     if lsof -i tcp:8001;then
