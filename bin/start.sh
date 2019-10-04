3f6169e050acdc7ea097b27b8d6dfa37c5e1b257
huangzhongzhen <1013452861@qq.com>
2019-10-04 13:45:37 +0800

    hzz

--- bin/start.sh
+++ bin/start.sh
@@ -2,6 +2,14 @@
 if [ -d "/opt/node-server/build" ] && [ -f "/opt/node-server/build/index.js" ];then
     cd '/opt/node-server'
     cnpm i
+    if ps -ef|grep mongod;then
+        echo 'restart mongod'
+        mongod --shutdown
+        mongod --auth &
+    else
+        echo 'start mongod'
+        mongod --auth &
+    fi
     if lsof -i tcp:8001;then
 	    echo 'restart node-server'
         pm2 restart "./build/index.js"
